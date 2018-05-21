c7909f0e8a58b93c93cfacdb1bf2990b0cf1df7e
Edmond Chow <edmond@apiary>
2018-05-21 17:06:28 -0400

    Anton's changes in Edmond's office

--- driv2.m
+++ driv2.m
@@ -2,35 +2,50 @@ function driv
 % Homotopy continuation for power flow for s=1 connected component
 
 % matrix A dimensions is n by n
-n = 7;
+n = 15;
 
 % construct sparse A0 and A1 with random values
 rng(0);
-A0 = spdiags(rand(n,2),0:1,n,n);
-A0(n,1) = rand;
-A1 = spdiags(rand(n,2),0:1,n,n);
-A1(n,1) = rand;
+A0 = spdiags(my_random_matrix(n,2),0:1,n,n);
+A0(n,1) = my_random_number;
+ep = 0.05;
+A1 = A0 + ep*spdiags(my_random_matrix(n,2),0:1,n,n);
+A1(n,1) = A0(n,1) + ep*my_random_number;
 
 %A0 = [1 .5 0; 0 1 .5; .5 0 1];
 %A1 = [1  1 0; 0 1  1;  1 0 1];
 %n = length(A0);
 
 % choose a solution
-x = rand(n,1);
-y = rand(n,1);
-y(n) = 0;
+x0 = my_random_matrix(n,1);
+y0 = my_random_matrix(n,1);
+y0(n) = 0;
+
+zero_b = zeros(2*n,1);
 
 % right-hand side for this solution
-b = F(x, y, A0, A1, zeros(2*n,1), 0);
+b0 = F(x0, y0, A0, A1, zero_b, zero_b, 0);
 
-% check initial solution is not a final solution
-F(x, y, A0, A1, b, 1);
+% choose a solution
+x1 = x0 + ep*my_random_matrix(n,1);
+y1 = y0 + ep*my_random_matrix(n,1);
+y1(n) = 0;
+
+% right-hand side for this solution
+b1 = F(x1, y1, A0, A1, zero_b, zero_b, 1);
+
+A0
+A1
+b0
+b1
+
+x = x0; y = y0;
 
 % check rank
 J = Jac(x, y, A0, A1, 0.1);
 svd(J);
 
-numsteps = 5;
+numsteps = 100;
 deltat = 1/numsteps;
 t = 0;
 
@@ -44,23 +59,30 @@ for step = 1:numsteps
   t = t + deltat;
   
   fprintf('======== time step %d: %f =========\n', step, t);
-  fprintf('After predict: %f\n', norm(F(x, y, A0, A1, b, t)));
+  fprintf('After predict: %f\n', norm(F(x, y, A0, A1, b0, b1, t)));
 
   % corrector using Gauss-Newton iterations
-  for iter = 1:5
+  for iter = 1:10
     J = Jac(x, y, A0, A1, t);
-    f = F(x, y, A0, A1, b, t);
+    f = F(x, y, A0, A1, b0, b1, t);
     inc = -J(:,1:end-1) \ f;
-    % disp(norm(inc))
     x = x + inc(1:n);
     y(1:n-1) = y(1:n-1) + inc(n+1:end);
-    fprintf('Newton step %d: %f\n', iter, norm(F(x, y, A0, A1, b, t)));
+    res = norm(F(x, y, A0, A1, b0, b1, t));
+    fprintf('Newton step %d: %f\n', iter, res);
   end
+  norm_inc = norm(inc); 
+  if norm_inc > 0.0001 
+     fprintf('|inc| is HUGE!!!\n');
+     svd(J)
+     return;
+  end 
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function f = F(x, y, A0, A1, b, t)
+function f = F(x, y, A0, A1, b0, b1, t)
 C = (1-t)*A0 + t*A1;
+b = (1-t)*b0 + t*b1;
 n = length(C);
 z = x + i*y;
 f = diag(z)*(C*conj(z)) - (b(1:n)+i*b(n+1:end));
@@ -82,3 +104,11 @@ Cy = C*y;
 J = [diag(x)*C+diag(Cx)    diag(y)*C+diag(Cy)
      diag(y)*C-diag(Cy)   -diag(x)*C+diag(Cx)];
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5555555%%%%%%%%%%%%%%%%%%%%%%
+function r = my_random_number();
+r = rand * 2 - 1;
+function r = my_random_matrix(m,n);
+r = rand(m,n) * 2 - 1;
+
+
+   
